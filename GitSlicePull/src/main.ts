cdf635b2ae989ca45ea7ba31c48143c1729ed7c7
wutania <wutania@gmail.com>
2021-10-14 15:25:31 +0000

    rmat and build

--- GitSlicePull/src/main.ts
+++ GitSlicePull/src/main.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core'
-import { context } from '@actions/github'
+import {context} from '@actions/github'
 import axios from 'axios'
-import { promises as fs } from 'fs'
+import {promises as fs} from 'fs'
 
 process.on('unhandledRejection', handleError)
 main().catch(handleError)
